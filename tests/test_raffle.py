f902afade843acc8dc98c81a85656891d145f6db
Gajaraj Sekar <gajaraj@fathomx.co>
2023-11-20 18:45:19 +0800

    create tests

---
+++ tests/test_raffle.py
@@ -0,0 +1,13 @@
+# tests/test_raffle.py
+
+from typer.testing import CliRunner
+
+from raffle_draw_game import __app_name__, __version__, cli
+
+runner = CliRunner()
+
+
+def test_version():
+    result = runner.invoke(cli.app, ["--version"])
+    assert result.exit_code == 0
+    assert f"{__app_name__} v{__version__}\n" in result.stdout
